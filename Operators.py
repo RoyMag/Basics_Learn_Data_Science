a5a55771c678de2c575c0155c490850c5104243f
Subhadeep Roy <subhadeeproy29@gmail.com>
2020-06-29 21:42:01 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  # Arithmetic Operators
+
+# In[3]:
+
+
+# Addition
+
+3 + 5
+
+
+# In[4]:
+
+
+# Subtraction
+
+54 - 46
+
+
+# In[6]:
+
+
+# Multiplication
+
+2 * 3
+
+
+# In[8]:
+
+
+# Division
+
+35 / 7
+
+
+# In[10]:
+
+
+"roy"+20
+
+
+# In[12]:
+
+
+"roy"+"20"
+
+
+# In[14]:
+
+
+"roy"*5
+
+
+# # Comparison Operators
+
+# In[16]:
+
+
+# greater than
+
+45 > 34
+
+
+# In[17]:
+
+
+# less than
+
+56 < 23
+
+
+# In[19]:
+
+
+34 * 34 < 45 * 23
+
+
+# In[21]:
+
+
+45 == 45
+
+
+# # Logical Operators
+
+# In[23]:
+
+
+# and operator
+
+0 and 3
+
+
+# In[25]:
+
+
+3 and 0
+
+
+# In[26]:
+
+
+3 and 5
+
+
+# In[27]:
+
+
+# or operator
+
+0 or 3
+
+
+# In[28]:
+
+
+3 or 5
+
+
+# In[30]:
+
+
+# not operator
+
+not 0
+
+
+# In[33]:
+
+
+not "roy"
+
